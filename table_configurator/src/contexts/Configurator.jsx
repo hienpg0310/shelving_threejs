c6085d64a0c04d8c4a905206b3048635bf989bb5
Ghienn <93633153+Ghienn@users.noreply.github.com>
2023-02-27 15:23:52 +0700

    update

---
+++ table_configurator/src/contexts/Configurator.jsx
@@ -0,0 +1,34 @@
+import { createContext, useContext, useState } from "react";
+
+const ConfiguratorContext = createContext();
+
+export const ConfiguratorProvider = ({ children }) => {
+  const [legs, setLegs] = useState(0);
+  const [legsColor, setLegsColor] = useState("#777777");
+  const [tableWidth, setTableWidth] = useState(100);
+  // const [tableThickness, settableThickness] = useState(10);
+  const [plateColor, setPlateColor] = useState("#ECECEC");
+
+  return (
+    <ConfiguratorContext.Provider
+      value={{
+        legs,
+        setLegs,
+        legsColor,
+        setLegsColor,
+        tableWidth,
+        setTableWidth,
+        plateColor,
+        setPlateColor,
+        // tableThickness, 
+        // settableThickness,
+      }}
+    >
+      {children}
+    </ConfiguratorContext.Provider>
+  );
+};
+
+export const useConfigurator = () => {
+  return useContext(ConfiguratorContext);
+};
